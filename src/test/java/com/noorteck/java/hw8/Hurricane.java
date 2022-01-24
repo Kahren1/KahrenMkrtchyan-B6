93031c3a5fc5973ea802b26f98fc10d1e3cd1e8f
Kahren1 <kahren.mkrtchyan@gmail.com>
2022-01-24 14:48:09 -0500

    another try

--- src/test/java/com/noorteck/java/hw8/Hurricane.java
+++ src/test/java/com/noorteck/java/hw8/Hurricane.java
@@ -16,7 +16,7 @@ public class Hurricane {
 				System.out.println("Cat 4 hurricane, wind speed: "+i);
 			else if(i>=165&&i<=194)
 				System.out.println("Cat 5 hurricane, wind speed: "+i);
-		}
+		 }
 
 	}
 }
