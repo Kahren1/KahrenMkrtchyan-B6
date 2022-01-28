c191dc21e5561f6e9a31efbdeece4e42c25d7720
Kahren1 <kahren.mkrtchyan@gmail.com>
2022-01-27 21:27:09 -0500

    retry

---
+++ src/test/java/com/noorteck/java/hw06/Day6Demo.java
@@ -0,0 +1,13 @@
+package com.noorteck.java.hw06;
+
+public class Day6Demo {
+	
+		public static void main(String[] args) {
+			System.out.println("Test 101");
+			System.out.println("***");
+		}
+
+	
+
+
+}
