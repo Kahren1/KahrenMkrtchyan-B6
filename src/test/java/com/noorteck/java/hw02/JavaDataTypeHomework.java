16dd8df9e3c8dc6e95c3235bb882bd06a92d7111
Kahren1 <kahren.mkrtchyan@gmail.com>
2022-01-27 21:08:29 -0500

    commit after changing the package names

---
+++ src/test/java/com/noorteck/java/hw02/JavaDataTypeHomework.java
@@ -0,0 +1,22 @@
+package com.noorteck.java.hw02;
+
+class JavaDataTypeHomework{
+	
+	public static void main(String[] args) {
+		int var = 10;
+		System.out.println(var);
+		
+		double var2=1098.34354;
+		System.out.println(var2);
+		
+		char var3 = 'F';
+		System.out.println(var3);
+		
+		String var4 = "United States";
+		System.out.println(var4);
+		
+		boolean var5 = false;
+		System.out.println(var5);
+	}
+
+}
